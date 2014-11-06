5dd0737508031b188c43d8e813eff5e2df053aaf
Rodolfo Dagum <rdagum@convey.com>
2014-11-06 11:49:25 -0600

    R-0 Added conditional define to compile in console mode

--- convey-src/uCnvDunitUtilities.pas
+++ convey-src/uCnvDunitUtilities.pas
@@ -8,7 +8,9 @@ implementation
 
 uses
   Forms,
+  {$IFNDEF CONSOLE_TESTRUNNER}
   Console,
+  {$EndIF}
   SysUtils,
   TestFramework,
   GUITestRunner,
@@ -18,7 +20,9 @@ uses
 procedure RunDunitApp;
 var
   TestForm : TGUITestRunner;
+  {$IFNDEF CONSOLE_TESTRUNNER}
   Cons : TStdConsole;
+  {$EndIf}
 begin
   if (ParamCount >= 1) and (ParamStr(1) = '-console') then
     IsConsole := True;
@@ -27,15 +31,19 @@ begin
     RegisteredTests.LoadConfiguration(ParamStr(3), False, False);
   if IsConsole and (ParamCount >= 2) then
     begin
+      {$IFNDEF CONSOLE_TESTRUNNER}
       Cons := TStdConsole.Create(nil);
       try
         Cons.Open;
+      {$ENDIF}
         TestFramework.RunTest(RegisteredTests, [
           TXMLTestListener.Create(ExpandFileName(ParamStr(2))) as ITestListener,
           TGTestStyleTestListener.Create as ITestListener]);
+      {$IFNDEF CONSOLE_TESTRUNNER}
       finally
         Cons.Free;
       end;
+      {$ENDIF}
     end
   else
     begin
